8698509293a5e97adfa3d18787e9b54fdcefa95a
devdivdev <abdel.hamdani213@gmail.com>
2018-11-22 14:36:22 +0100

    Firebase

---
+++ app/src/main/java/space/majid/testing/materieldestesting/MainActivity.kt
@@ -0,0 +1,13 @@
+package space.majid.testing.materieldestesting
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
