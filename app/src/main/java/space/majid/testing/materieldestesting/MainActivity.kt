2725219e64aeda81e8c21d934044cb79edc1581a
devdivdev <abdel.hamdani213@gmail.com>
2018-11-23 18:25:44 +0100

    -Added: Login Form

--- app/src/main/java/space/majid/testing/materieldestesting/MainActivity.kt
+++ app/src/main/java/space/majid/testing/materieldestesting/MainActivity.kt
@@ -1,11 +1,33 @@
 package space.majid.testing.materieldestesting
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener {
+            val email = login_email.text.toString()
+            val password = login_pass.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("FIRE_BASE", "signInWithEmail:success")
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d("FIRE_BASE", "signInWithEmail:failure", task.exception)
+                        }
+                    }
+        }
+
     }
 }
