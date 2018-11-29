db6d580433c239108cbff0292a4912d483777dde
devdivdev <abdel.hamdani213@gmail.com>
2018-11-29 15:37:45 +0100

    Added: Firebase Realtime database to save new users

--- app/src/main/java/space/majid/testing/materieldestesting/MainActivity.kt
+++ app/src/main/java/space/majid/testing/materieldestesting/MainActivity.kt
@@ -1,44 +1,46 @@
 package space.majid.testing.materieldestesting
 
-import android.content.Intent
+
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.startActivityForResult
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
-        auth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
         setContentView(R.layout.activity_main)
-
-
         // Login button
         login_button.setOnClickListener {
             val email = login_email.text.toString()
             val password = login_pass.text.toString()
-
-            auth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("FIRE_BASE", "signInWithEmail:success")
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.d("FIRE_BASE", "signInWithEmail:failure", task.exception)
-                        }
-                    }
+            // TODO validation of these values
+            signInUser(email, password)
         }
 
         // Register Link
-
         register_link.setOnClickListener {
             Log.d("REGISTER_LINK", "clickListen:success")
-            val mess = "testing message"
-            startActivityForResult<RegisterActivity>(42)
+            startActivity<RegisterActivity>()
         }
-
     }
-
+    private fun signInUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("FIRE_BASE", "signInWithEmail:success")
+                        Log.d("FIRE_BASE_USER", it.result?.user?.uid)
+                        // TODO save user to database too with username
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.d("FIRE_BASE", "signInWithEmail:failure", it.exception)
+                    }
+                }
+    }
+}
--- app/src/main/java/space/majid/testing/materieldestesting/RegisterActivity.kt
+++ app/src/main/java/space/majid/testing/materieldestesting/RegisterActivity.kt
@@ -2,15 +2,66 @@ package space.majid.testing.materieldestesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    private lateinit var usersRef: DatabaseReference
 
     companion object {
         const val EXTRA_MESSAGE: String = "RegisterActivity.MESSAGE"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        usersRef = database.getReference("users")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        register_button.setOnClickListener {
+            val username = register_username.text.toString()
+            val email = register_email.text.toString()
+            val password = register_pass.text.toString()
+            val user = User(username, email, password)
+            // TODO make sure to validate these values
+            createNewUser(user)
+        }
     }
+
+    private fun createNewUser(user: User) {
+        auth.createUserWithEmailAndPassword(user.email, user.password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("FIRE_BASE", "createUserWithEmailAndPassword:success")
+                        // Saving the user to the database
+                        val newUser = auth.currentUser
+                        Log.d("FIRE_BASE", "User ID = ${newUser?.uid}")
+                        usersRef.child(newUser!!.uid).setValue(user)
+                                .addOnSuccessListener {
+                                    Log.d("FIRE_BASE", "usersaved:success")
+                                }
+                                .addOnFailureListener {
+                                    Log.d("FIRE_BASE", "usersaved:failure")
+                                    Log.d("FIRE_BASE", it.message)
+                                }
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.d("FIRE_BASE", "createUserWithEmailAndPassword:failure", task.exception)
+                    }
+                }
+    }
+}
+
+class User(var username: String, var email: String, var password: String) {
 }
